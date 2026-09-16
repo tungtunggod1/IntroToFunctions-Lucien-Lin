b9b3e6b0aaf81785c7737236b9e79da518ce7a5f
tungtunggod1 <lucienlin936@gmail.com>
2026-09-16 09:57:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-import turtle
+""" import turtle
 from turtle import *
 t = Turtle()
 
 t.shape('turtle')
-def square(x) :
-    
+def rectangle(x):
+    t.forward(x + 25)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x + 25)
+    t.left(90)
+    t.forward(x)
+rectangle(100)
+
+
+turtle.done() """
+
+
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(90)
 
 
 turtle.done()
